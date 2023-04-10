624189cbf75241db4050b2ace8a6defb96451a93
Hiroshi Murakami <piroz@users.noreply.github.com>
2023-04-11 06:14:39 +0900

    use openai

--- agent.py
+++ agent.py
@@ -1,18 +1,23 @@
+import os
 import transformers
 import torch
 import fire
+from langchain.llms.openai import OpenAI
 
 class Agent:
     def __init__(self):
+        self.llm = OpenAI(temperature=0, top_p=0.9, frequency_penalty=0.0, presence_penalty=0.0)
         pass
 
     def start(self):
         user_input = input("何か入力してください: ")
-        model = transformers.AutoModelForQuestionAnswering.from_pretrained('bert-large-uncased-whole-word-masking-finetuned-squad')
-        tokenizer = transformers.AutoTokenizer.from_pretrained("bert-large-uncased-whole-word-masking-finetuned-squad")
+        return self.llm.generate([user_input])
 
 
 def main():
+    if os.environ["OPENAI_API_KEY"] is None:
+        raise KeyError("OPENAI_API_KEY is not set")
+    
     agent = Agent()
     result = agent.start()
     print(result)
