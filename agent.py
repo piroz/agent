5b65f7409f2fe3398c1be141a63f288baca13e89
Hiroshi Murakami <piroz@users.noreply.github.com>
2023-04-12 05:39:04 +0900

    ZERO_SHOT_REACT_DESCRIPTION

--- agent.py
+++ agent.py
@@ -1,26 +1,29 @@
-import os
-import transformers
-import torch
 import fire
-from langchain.llms.openai import OpenAI
+from langchain.agents import load_tools
+from langchain.agents import initialize_agent
+from langchain.agents import AgentType
+from langchain.llms import OpenAI
+from dotenv import load_dotenv
 
-class Agent:
-    def __init__(self):
-        self.llm = OpenAI(temperature=0, top_p=0.9, frequency_penalty=0.0, presence_penalty=0.0)
-        pass
+load_dotenv()
 
-    def start(self):
-        user_input = input("何か入力してください: ")
-        return self.llm.generate([user_input])
+class ZeroShotReactDescriptionAgent:
+    def __init__(self, llm):
+        self.llm = llm
 
+    def run(self, prompt):
+        tools = load_tools(["serpapi", "llm-math"], llm=self.llm) # todo create other tools (ex. pinterest search)
+        agent = initialize_agent(tools, self.llm, agent=AgentType.ZERO_SHOT_REACT_DESCRIPTION, verbose=True)
+        agent.run(prompt)
 
 def main():
-    if os.environ["OPENAI_API_KEY"] is None:
-        raise KeyError("OPENAI_API_KEY is not set")
-    
-    agent = Agent()
-    result = agent.start()
-    print(result)
-
+    llm = OpenAI(temperature=0)
+    agent = ZeroShotReactDescriptionAgent(llm)
+    print("こんにちは！私は知識を持っています。")
+    print("私はあなたの質問に答えることができます。")
+    print("例えば、")
+    print("日本で利用されているSNSの上位５つのユーザ数はそれぞれ何人ですか？そのユーザー数の合計を求めてください。")
+    prompt = input("質問を入力してください: ")
+    agent.run(prompt)
 
 fire.Fire(main)
